8da7d5c988c1dd52df36d4e81055f3d2f4844742
WooKeyWallet <wookey@tutanota.com>
2019-07-03 16:02:09 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+current_path=`pwd`
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install wget curl vim git zip -y
 sudo apt-get install build-essential cmake tofrodos libtool-bin autoconf pkg-config -y
+ubuntu_version=`sudo lsb_release -r|awk '{print $2}'`
+ [ $ubuntu_version == 18.04 ] && sudo apt-get install python
 sudo mkdir -p /opt/android
 sudo chown `whoami` /opt/android
 cd /opt/android
@@ -13,3 +16,15 @@ cd /tmp/monero-android-lib/external-libs/
 find . -name "*.sh" -exec chmod +x {} \;
 mkdir -p build/src
 make all
+if [ $? -eq 0 ]; then
+cd $current_path
+mkdir -p monero-libs/{monero,boost,openssl,libsodium}/lib/{arm64-v8a,armeabi-v7a}
+cp -a /tmp/monero-android-lib/external-libs/build/build/monero/arm/lib/* monero-libs/monero/lib/armeabi-v7a/
+cp -a /tmp/monero-android-lib/external-libs/build/build/monero/arm64/lib/* monero-libs/monero/lib/arm64-v8a/
+cp -a /tmp/monero-android-lib/external-libs/build/build/boost/arm/lib/* monero-libs/boost/lib/armeabi-v7a/
+cp -a /tmp/monero-android-lib/external-libs/build/build/boost/arm64/lib/* monero-libs/boost/lib/arm64-v8a/
+cp -a /tmp/monero-android-lib/external-libs/build/build/openssl/arm/lib/* monero-libs/openssl/lib/armeabi-v7a/
+cp -a /tmp/monero-android-lib/external-libs/build/build/openssl/arm64/lib/* monero-libs/openssl/lib/arm64-v8a/
+cp -a /tmp/monero-android-lib/external-libs/build/build/libsodium/arm/lib/* monero-libs/libsodium/lib/armeabi-v7a/
+cp -a /tmp/monero-android-lib/external-libs/build/build/libsodium/arm64/lib/* monero-libs/libsodium/lib/arm64-v8a/
+fi
